3cebcdec8a80f90f1c29de6c4d344734890456e6
Marcusjenks <me@marcusjenks.com>
2018-03-20 16:02:03 -0400

    Basic implementation for random magic8ball response

---
+++ src/magic8Ball/Main.java
@@ -0,0 +1,31 @@
+package magic8Ball;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Give me a yes or no question and I shall answer it!");
+		Scanner question = new Scanner(System.in);
+		String input = question.nextLine();
+		
+		question.close();
+		
+		
+		int response = (int)(Math.random() * 4 + 1);
+		
+		switch(response) {
+		case 1: System.out.println("Most definetely yes!");
+				break;
+		case 2: System.out.println("For certain: NO!");
+				break;
+		case 3:	System.out.println("I'm too tired to calculate an answer");
+				break;
+		case 4:	System.out.println("uhhh..yeah..sure..");
+				break;
+		}
+				
+
+	}
+
+}
