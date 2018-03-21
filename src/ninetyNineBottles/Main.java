7e3f8714838f8f29a3967107f39d29f500903670
Marcusjenks <me@marcusjenks.com>
2018-03-21 19:57:10 -0400

    Added basic 99 bottles countdown

---
+++ src/ninetyNineBottles/Main.java
@@ -0,0 +1,29 @@
+package ninetyNineBottles;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		for (int i = 99; i >= 0; i--) {
+			switch (i) {
+			case 0:
+				return;
+			case 1:
+				System.out.println(i + " bottle of beer on the wall! " + i + " one bottle of beer!");
+				break;
+			default:
+				System.out.println(i + " bottles of beer on the wall! " + i + " bottles of beer!");
+				break;
+			}
+			if (i == 1) {
+				System.out.println("Take one down pass it around, there are no more bottles of beer on the wall!\n");
+			}
+			else if (i == 2) {
+				System.out.println("Take one down pass it around, " + (i - 1) + " bottle of beer on the wall!\n");
+			}
+			else
+				System.out.println("Take one down pass it around, " + (i - 1) + " bottles of beer on the wall!\n");
+		}
+	}
+
+}
